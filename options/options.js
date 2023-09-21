835dc0722c416038f87b3553f7ebc84b9f1a93b1
kelvin4664 <kelvinadigwu@yahoo.com>
2023-09-21 20:39:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-pond-3",
+  url: "https://long-pond-3.botics.co",
+  api: "https://long-pond-3.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
